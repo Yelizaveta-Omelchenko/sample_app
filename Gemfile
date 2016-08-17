69033abb27e4ea712f3e0a20bc654e40d3975ec8
Yelizaveta-Omelchenko <yelizaveta.omelchenko@gmail.com>
2016-08-17 11:39:39 +0300

    Finish layout and routes

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@ source 'https://rubygems.org'
 source 'https://rubygems.org'
 
 gem 'rails',        '5.0.0'
+gem 'bootstrap-sass', '3.3.6'
+gem 'autoprefixer-rails'
 gem 'puma',         '3.4.0'
 gem 'sass-rails',   '5.0.6'
 gem 'uglifier',     '3.0.0'
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
-  get 'static_pags/about'
 
-  get 'static_pags/home'
 
-  get 'static_pags/help'
-
-  get 'static_pags/contact'
+  root 'static_pags#home'
+  get  '/help',    to: 'static_pags#help'
+  get  '/about',   to: 'static_pags#about'
+  get  '/contact', to: 'static_pags#contact'
+  get '/signup',to: 'users#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
-  root 'static_pags#home'
+
 
 
 end
